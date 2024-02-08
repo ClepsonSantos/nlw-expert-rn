1a053d064af129d3d57bebe3d74319251c0d9c03
Clepson Lacerda <clepsonsantos9@gmail.com>
2024-02-08 14:50:39 -0300

    concluído a funcionalidade do envio do pedido.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Feather } from "@expo/vector-icons";
+import { Link } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import colors from "tailwindcss/colors";
 
@@ -19,17 +20,19 @@ export function Header({ title, cardQuantityItems = 0 }: HeaderProps) {
                     {title}
                 </Text>
             </View>
-            
-            {cardQuantityItems > 0 &&
-                <TouchableOpacity className="relative" activeOpacity={0.7}>
-                    <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
-                        <Text className="text-slate-900 font-bold text-xs">
-                            {cardQuantityItems}
-                        </Text>
-                    </View>
 
-                    <Feather name="shopping-bag" color={colors.white} size={24} />
-                </TouchableOpacity>
+            {cardQuantityItems > 0 &&
+                <Link href="/cart" asChild>
+                    <TouchableOpacity className="relative" activeOpacity={0.7}>
+                        <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
+                            <Text className="text-slate-900 font-bold text-xs">
+                                {cardQuantityItems}
+                            </Text>
+                        </View>
+
+                        <Feather name="shopping-bag" color={colors.white} size={24} />
+                    </TouchableOpacity>
+                </Link>
             }
         </View>
     );
